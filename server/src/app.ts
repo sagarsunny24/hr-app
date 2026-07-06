354bd2605c6429963409f415ab3cb17ae7f3e0eb
Sagar Sunny <sagarsunny2403@gmail.com>
2026-07-06 13:04:29 +0530

    backend: added cookie parser middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import httpLogger from '../utils/morgan.js'
 import { corsOptions } from './config/corsOptions.js'
+import cookieParser from 'cookie-parser'
 
 
 
@@ -10,3 +11,4 @@ export const app = express()
 app.use(cors(corsOptions))
 app.use(express.json())
 app.use(httpLogger)
+app.use(cookieParser())
