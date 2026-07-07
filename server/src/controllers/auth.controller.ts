e75f44e91384b8e99580a329c0570453b75ebd05
Sagar Sunny <sagarsunny2403@gmail.com>
2026-07-07 11:14:55 +0530

    backend:added register/company route and controller

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,29 +1,50 @@
-import {  RequestHandler } from "express";
+import { RequestHandler } from "express";
 import { AppDataSource } from "@/config/db.js";
 import { Companies } from "@/entities/Companies.js";
+import { Employee } from "@/entities/Employee.js";
 
+const registerCompany: RequestHandler = async (req, res, next) => {
+  const queryRunner = AppDataSource.createQueryRunner()
+  await queryRunner.connect()
+  await queryRunner.startTransaction()
+  try {
+    const {
+      emp_name,emp_email,emp_phone,emp_dept,emp_role,emp_joining_date,emp_status,
+      company_name,
+      registration_no,
+      ceo_name,
+      company_loc,
+      created_date,
+      company_address,
+    } = req.body;
 
-const registerCompany:RequestHandler =async(req,res,next)=>{
+   const companyRepo = queryRunner.manager.getRepository(Companies)
+   const employeeRepo = queryRunner.manager.getRepository(Employee)
 
-  try{
- const {company_name, registration_no ,ceo_name, company_loc,created_date,company_address} = req.body
-
-  await AppDataSource
-        .createQueryBuilder()
-        .insert()
-        .into(Companies)
-        .values({
-          company_name,
-          registration_no,
-          ceo_name,
-          company_loc,
-          created_date,
-          company_address
-        })
-        .execute()
-  res.status(201).json({message:'Company successfully created'})
+  const company = await companyRepo.save({
+    company_name,
+      registration_no,
+      ceo_name,
+      company_loc,
+      created_date,
+      company_address,
+  })
+  const employee = await employeeRepo.save({
+    emp_name,emp_email,emp_phone,emp_dept,emp_role,emp_joining_date,emp_status,company
+  })
+ await queryRunner.commitTransaction()
+    
+    res.status(201).json({ message: "Company successfully created" });
+  } catch (err) {
+    await queryRunner.rollbackTransaction()
+    next(err);
   }
- catch(err){
-  next(err)
- }
-}
+  finally{
+await queryRunner.release()
+  }
+};
+
+
+
+
+export {registerCompany}
--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,14 +1,13 @@
 import express from 'express'
-
+import { registerCompany } from '@/controllers/auth.controller.js';
 const router = express.Router();
 
 router.post('/register/company',registerCompany)
-router.post('/hr/register',registerHR)
-router.post('/hr/login',loginHR)
-router.post('/employee/register'.registerEmployee)
-router.post('/employee/login',loginEmployee)
-router.post('/hr/logout',logoutHR)
-router.post('employee/logout',logoutEmployee)
-router.get('/refresh',handleRefresh)
+// router.post('/hr/register',registerHR)
+// router.post('/employee/register'.registerEmployee)
+// router.post('/employee/login',loginEmployee)
+// router.post('/hr/logout',logoutHR)
+// router.post('employee/logout',logoutEmployee)
+// router.get('/refresh',handleRefresh)
 
 export default router
