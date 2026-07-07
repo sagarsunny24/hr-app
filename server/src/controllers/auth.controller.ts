8c69413e80ed04b9d61abf8026f9e3bde3830a5a
Sagar Sunny <sagarsunny2403@gmail.com>
2026-07-07 11:31:55 +0530

    backend:fixed register/company to automatically create user 0

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -2,49 +2,72 @@ import { RequestHandler } from "express";
 import { AppDataSource } from "@/config/db.js";
 import { Companies } from "@/entities/Companies.js";
 import { Employee } from "@/entities/Employee.js";
+import { Users } from "@/entities/Users.js";
+import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken'
 
 const registerCompany: RequestHandler = async (req, res, next) => {
-  const queryRunner = AppDataSource.createQueryRunner()
-  await queryRunner.connect()
-  await queryRunner.startTransaction()
+  const queryRunner = AppDataSource.createQueryRunner();
+  await queryRunner.connect();
+  await queryRunner.startTransaction();
   try {
     const {
-      emp_name,emp_email,emp_phone,emp_dept,emp_role,emp_joining_date,emp_status,
+      emp_name,
+      emp_email,
+      emp_phone,
+      emp_dept,
+      emp_role,
+      emp_joining_date,
+      emp_status,
       company_name,
       registration_no,
       ceo_name,
       company_loc,
       created_date,
       company_address,
+      password,
     } = req.body;
 
-   const companyRepo = queryRunner.manager.getRepository(Companies)
-   const employeeRepo = queryRunner.manager.getRepository(Employee)
+    const companyRepo = queryRunner.manager.getRepository(Companies);
+    const employeeRepo = queryRunner.manager.getRepository(Employee);
+    const userRepo = queryRunner.manager.getRepository(Users);
 
-  const company = await companyRepo.save({
-    company_name,
+    const company = await companyRepo.save({
+      company_name,
       registration_no,
       ceo_name,
       company_loc,
       created_date,
       company_address,
-  })
-  const employee = await employeeRepo.save({
-    emp_name,emp_email,emp_phone,emp_dept,emp_role,emp_joining_date,emp_status,company
-  })
- await queryRunner.commitTransaction()
-    
-    res.status(201).json({ message: "Company successfully created" });
+    });
+    const employee = await employeeRepo.save({
+      emp_name,
+      emp_email,
+      emp_phone,
+      emp_dept,
+      emp_role,
+      emp_joining_date,
+      emp_status,
+      company,
+    });
+    const hashedPWD = await bcrypt.hash(password, 10);
+    await userRepo.save({
+      user: employee,
+      email: emp_email,
+      password_hash: hashedPWD,
+    });
+    await queryRunner.commitTransaction();
+  
+    res
+      .status(201)
+      .json({ message: "Company successfully created" });
   } catch (err) {
-    await queryRunner.rollbackTransaction()
+    await queryRunner.rollbackTransaction();
     next(err);
-  }
-  finally{
-await queryRunner.release()
+  } finally {
+    await queryRunner.release();
   }
 };
 
 
-
-
-export {registerCompany}
+export { registerCompany };
--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -3,9 +3,10 @@ import { registerCompany } from '@/controllers/auth.controller.js';
 const router = express.Router();
 
 router.post('/register/company',registerCompany)
+// router.post('/login',loginUser)
 // router.post('/hr/register',registerHR)
 // router.post('/employee/register'.registerEmployee)
-// router.post('/employee/login',loginEmployee)
+
 // router.post('/hr/logout',logoutHR)
 // router.post('employee/logout',logoutEmployee)
 // router.get('/refresh',handleRefresh)
