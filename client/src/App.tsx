a06387b429b6ccfc0752ba14a0ecd89c5c6c332b
Sagar Sunny <sagarsunny2403@gmail.com>
2026-07-17 12:48:05 +0530

    fronten: added dark and light theme configs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,25 @@
 import { darkTheme,lightTheme } from "./theme/theme"
-import { ThemeProvider } from "@mui/material"
+import { ThemeProvider, type Theme } from "@mui/material"
 
 import LoginPage from "./pages/LoginPage"
+import { useEffect, useState } from "react"
 
 export default function App() {
+  const [windowTheme, setWindowTheme] = useState(()=>{
+    const savedTheme = localStorage.getItem('theme')
+    return savedTheme? JSON.parse(savedTheme) : lightTheme
+  })
+  useEffect(()=>{
+    localStorage.setItem('theme',JSON.stringify(windowTheme))
+  },[windowTheme])
 
+  function toggleTheme (){
+   setWindowTheme((prev:Theme) => {
+    return prev === lightTheme ? darkTheme : lightTheme;
+  });
+  }
   return (
-    <ThemeProvider theme={lightTheme}>
+    <ThemeProvider theme={windowTheme}>
   <LoginPage />
     </ThemeProvider>
   
